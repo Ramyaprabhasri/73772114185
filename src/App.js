61f0c66c81c2ce424723f1127b16ac091ae5af81
Ramyaprabhasri <ramyaprabhasrivr@gmail.com>
2023-12-08 10:48:18 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <ul>
+          <li>Name: Ramyaprabhasri V R</li>
+          <li>Dept: Computer Science and Engineering</li>
+          <li>Year: III</li>
+          <li>Reg_no: 73772114185</li>
+          <li>Area of Interest: Web Development</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
